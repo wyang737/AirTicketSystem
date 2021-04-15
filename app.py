eedbfd903149aadef484942b90c83303263e79b0
Winston Yang <wyang737@users.noreply.github.com>
2021-04-15 15:32:48 -0400

    not useful

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+# main page
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+# home page
+@app.route("/home")
+def home():
+	return render_template("home.html")
+
+# public searching page
+@app.route("/publicsearch")
+def publicsearch():
+	return render_template("publicsearch.html")
+
+#login page
+@app.route("/login", methods=["GET"])
+def login():
+	return render_template("login.html")
+
+# registration for new users
+@app.route("/register", methods=["GET"])
+def register():
+	return render_template("register.html")
+
+if __name__ == "__main__":
+	app.run(host = 'localhost', port = 5000, debug = True)
