3e9c2c0d02ce671fece5a2d2fddcbabe7953cdd6
Winston Yang <wyang737@users.noreply.github.com>
2021-04-15 15:53:33 -0400

    login?

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -18,8 +18,15 @@ def publicsearch():
 	return render_template("publicsearch.html")
 
 #login page
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+	error = None
+	if request.method == "POST":
+		# check the credentials here...
+		if request.form['username'] != 'root' or request.form['password'] != 'password':
+			error = 'Invalid username/password, please try again.'
+		else:
+			return redirect(url_for('index'))
 	return render_template("login.html")
 
 # registration for new users
