5977d054f4e9dadd8c41dd24013f6bb4f683e154
Winston Yang <wyang737@users.noreply.github.com>
2021-04-16 12:23:47 -0400

    customer, agent, staff functionality

--- project.py
+++ project.py
@@ -36,12 +36,58 @@ def register():
 	if request.method == "POST":
 		username = request.form['username']
 		password = request.form['password']
-		userType = request.form['userType']
+		userType = request.form['userType'] # customer, agent, staff
 		info = [username, password, userType]
 		print (info)
 		# if username & password are in the database, throw some kind of error
 		# otherwise, should add them as a tuple into the db
 	return render_template("register.html")
+
+@app.route("/customer")
+def customer():
+	return render_template("customer.html")
+
+@app.route("/agent")
+def agent():
+	return render_template("agent.html")
+
+@app.route("/staff")
+def staff():
+	return render_template("staff.html")
+
+@app.route("/addstuff", methods=["GET", "POST"])
+def addstuff():
+	error = None
+	if request.method == "POST":
+		info = []
+		if len(request.form) == 2:   # it's a new airport
+			name = request.form['airportName']
+			city = request.form['city']
+
+			# need to add to db now..
+		elif len(request.form) == 3: # it's a new airplane
+			airplaneID = request.form['airplaneID']
+			numSeats = request.form['numSeats']
+			name = request.form['airlineName']
+
+			# need to add to db now..
+		else:  						 # it's a new flight
+			airlineName = request.form['airlineName']
+			status = request.form['status'] # ontime or delayed
+			flightNumber = request.form['flightNumber']
+			depAirport = request.form['depAirport']
+			depDate = request.form['depDate'] # of the format 2021-04-22
+			depTime = request.form['depTime'] # of the format 00:00 - 23:59 
+			arrAirport = request.form['arrAirport']
+			arrDate = request.form['arrDate']
+			arrTime = request.form['arrTime']
+			price = request.form['basePrice']
+			airplaneID = request.form['airplaneID']
+
+			# need to add to db now..
+	print (request.form)
+	return render_template("addstuff.html")
+
 
 if __name__ == "__main__":
 	app.run(host = 'localhost', port = 5000, debug = True)
