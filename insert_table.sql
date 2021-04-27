a0164228d9eeddd6ad593a913e92326d47ace9ad
Winston Yang <wyang737@users.noreply.github.com>
2021-04-26 21:22:47 -0400

    NAME

--- insert_table.sql
+++ insert_table.sql
@@ -7,7 +7,7 @@ INSERT INTO customer VALUES ('Winston Yang', 'wy882@nyu.edu', 'leagueoflegends',
 INSERT INTO bookingagent VALUES ('sallyjane@gmail.com', 'bakecake98765', 12345, 65.2);
 INSERT INTO airplane VALUES (1234567890, 1000, 'China Eastern');
 INSERT INTO airplane VALUES (0987654321, 2345, 'China Eastern');
-INSERT INTO staff VALUES ('johnsmith@gmail.com', 'footballmaster', 'John', 'Smith', '1969-11-22', 9998887777, 'China Eastern');
+INSERT INTO staff VALUES ('johnsmith@gmail.com', 'footballmaster', 'John Smith', '1969-11-22', 9998887777, 'China Eastern');
 INSERT INTO flight VALUES ('China Eastern', 'Delayed', 123456789, 'JFK', '2021-03-30', '12:30:12', 'PVG', '2021-03-31', '08:30:59', 500, 1234567890);
 INSERT INTO flight VALUES ('China Eastern', 'Delayed', 234567890, 'JFK', '2021-04-30', '11:20:52', 'PVG', '2021-4-31', '11:44:29', 666, 1234567890);
 INSERT INTO flight VALUES ('China Eastern', 'On Time', 987654321, 'PVG', '2021-03-30', '11:10:09', 'JFK', '2021-04-01', '07:55:55', 420, 0987654321);
